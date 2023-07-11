7ebc746383038689fa87069f82562e1cf20cb590
andrew schwirzinski <aschwirzinski@gmail.com>
2023-07-11 12:39:12 -0400

    fixed errors

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		HourlyEmployee myHourlyEmp = new HourlyEmployee("Andrew","07/19/1980", "",4444444, "07/08/2001", 15.50);
+		HourlyEmployee myHourlyEmp = new HourlyEmployee("Andrew","07/19/1980", 000,"4444444",  15.50);
 		myHourlyEmp.getDoublePay();
 
 	}
