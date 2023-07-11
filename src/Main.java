284cb03b7642540ff5462362500f6dcb7a7f82cb
andrew schwirzinski <aschwirzinski@gmail.com>
2023-07-11 12:36:38 -0400

    OOP practice

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		HourlyEmployee myHourlyEmp = new HourlyEmployee("Andrew","07/19/1980", "",4444444, "07/08/2001", 15.50);
+		myHourlyEmp.getDoublePay();
+
 	}
+
+
+
 }
